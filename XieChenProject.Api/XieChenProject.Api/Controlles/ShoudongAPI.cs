f265ad89b8730b27a7682d590ec51156bd4d9db9
luo xiaodong <275611813@qq.com>
2021-03-04 14:28:58 +0800

    update api

---
+++ XieChenProject.Api/XieChenProject.Api/Controlles/ShoudongAPI.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace XieChenProject.Api.Controlles
+{
+    [Route("api/shoudongapi")]
+    public class ShoudongAPI:Controller
+    {
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+    }
+}
--- XieChenProject.Api/XieChenProject.Api/Startup.cs
+++ XieChenProject.Api/XieChenProject.Api/Startup.cs
@@ -16,6 +16,7 @@ namespace XieChenProject.Api
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddControllers();//×¢²áapi controllers ×é¼þ
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -26,15 +27,23 @@ namespace XieChenProject.Api
                 app.UseDeveloperExceptionPage();
             }
 
+
             app.UseRouting();
 
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Hello World!");
-                });
+                //endpoints.MapGet("/test", async context =>
+                //{
+                //    await context.Response.WriteAsync("Hello form Test!");
+                //});
+
+                //endpoints.MapGet("/", async context =>
+                //{
+                //    await context.Response.WriteAsync("Hello World!");
+                //});
+                endpoints.MapControllers();
             });
+
         }
     }
 }
